bdb555ffb2ffdafe07527866ae287e1e3a78b6a7
Manuel Cardoso <manuelcardosomasc@gmail.com>
2024-11-13 19:04:16 +0100

    food Card

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,21 +1,127 @@
+import { useState, useEffect } from "react";
 import FoodCard from "./components/FoodCard/FoodCard.jsx";
 
 function Layout() {
+  // State para armazenar a URL da imagem com base no tamanho da tela
+  const [screenSize, setScreenSize] = useState(window.innerWidth);
+
+  // Função para atualizar o tamanho da tela
+  const updateScreenSize = () => {
+    setScreenSize(window.innerWidth);
+  };
+
+  useEffect(() => {
+    // Adiciona o event listener para monitorar o redimensionamento da janela
+    window.addEventListener("resize", updateScreenSize);
+
+    // Limpeza do event listener
+    return () => {
+      window.removeEventListener("resize", updateScreenSize);
+    };
+  }, []);
+
+  // Função para escolher o link correto da imagem
+  const getImageLink = (image) => {
+    if (screenSize < 375) {
+      return image.mobile;
+    } else if (screenSize >= 375 && screenSize < 1440) {
+      return image.tablet;
+    } else {
+      return image.desktop;
+    }
+  };
+
+  // Imagens com links para diferentes tamanhos de tela
+  const images = [
+    {
+      mobile: "https://i.ibb.co/TKZFyS5/image-waffle-mobile.jpg",
+      tablet: "https://i.ibb.co/s6THN6f/image-waffle-tablet.jpg",
+      desktop: "https://i.ibb.co/qd6Q2CQ/image-waffle-desktop.jpg",
+      food_name: "apple pie",
+      food_full_name: "apple pie two",
+      food_price: "25",
+    },
+    {
+      mobile: "https://i.ibb.co/rtKZrsB/image-creme-brulee-mobile.jpg",
+      tablet: "https://i.ibb.co/Zg79S1D/image-creme-brulee-tablet.jpg",
+      desktop: "https://i.ibb.co/BswBJTX/image-creme-brulee-desktop.jpg",
+      food_name: "apple pie",
+      food_full_name: "apple pie two",
+      food_price: "25",
+    },
+    {
+      mobile: "https://i.ibb.co/mCzxVL2/image-macaron-mobile.jpg",
+      tablet: "https://i.ibb.co/4VFsccQ/image-macaron-tablet.jpg",
+      desktop: "https://i.ibb.co/6Ygj5Dj/image-macaron-desktop.jpg",
+      food_name: "apple pie",
+      food_full_name: "apple pie two",
+      food_price: "25",
+    },
+    {
+      mobile: "https://i.ibb.co/D1Vd2wz/image-tiramisu-mobile.jpg",
+      tablet: "https://i.ibb.co/JpfpxKW/image-tiramisu-tablet.jpg",
+      desktop: "https://i.ibb.co/W0QYkNG/image-tiramisu-desktop.jpg",
+      food_name: "apple pie",
+      food_full_name: "apple pie two",
+      food_price: "25",
+    },
+    {
+      mobile: "https://i.ibb.co/fFC5BqS/image-baklava-mobile.jpg",
+      tablet: "https://i.ibb.co/N3MBvJ7/image-baklava-tablet.jpg",
+      desktop: "https://i.ibb.co/rFND550/image-baklava-desktop.jpg",
+      food_name: "apple pie",
+      food_full_name: "apple pie two",
+      food_price: "25",
+    },
+    {
+      mobile: "https://i.ibb.co/TLbMK49/image-meringue-mobile.jpg",
+      tablet: "https://i.ibb.co/6Ndjnfw/image-meringue-tablet.jpg",
+      desktop: "https://i.ibb.co/p3CD3Dz/image-meringue-desktop.jpg",
+      food_name: "apple pie",
+      food_full_name: "apple pie two",
+      food_price: "25",
+    },
+    {
+      mobile: "https://i.ibb.co/tq3s0dN/image-cake-mobile.jpg",
+      tablet: "https://i.ibb.co/xHNJP5R/image-cake-tablet.jpg",
+      desktop: "https://i.ibb.co/5vk4wBT/image-cake-desktop.jpg",
+      food_name: "apple pie",
+      food_full_name: "apple pie two",
+      food_price: "25",
+    },
+    {
+      mobile: "https://i.ibb.co/WKz8dV1/image-brownie-mobile.jpg",
+      tablet: "https://i.ibb.co/gmRgfTc/image-brownie-tablet.jpg",
+      desktop: "https://i.ibb.co/MkRzFqd/image-brownie-desktop.jpg",
+      food_name: "apple pie",
+      food_full_name: "apple pie two",
+      food_price: "25",
+    },
+    {
+      mobile: "https://i.ibb.co/h1xrF3f/image-panna-cotta-mobile.jpg",
+      tablet: "https://i.ibb.co/ftFJLZm/image-panna-cotta-tablet.jpg",
+      desktop: "https://i.ibb.co/cgHPwX9/image-panna-cotta-desktop.jpg",
+      food_name: "apple pie",
+      food_full_name: "apple pie two",
+      food_price: "25",
+    },
+  ];
+
   return (
     <>
       <section className="left-side">
         <div className="food-container">
           <h1 className="food-container-title">Desserts</h1>
           <div className="food-content">
-            <FoodCard food_image="https://i.ibb.co/qd6Q2CQ/image-waffle-desktop.jpg" />
-            <FoodCard food_image="" />
-            <FoodCard food_image="" />
-            <FoodCard food_image="" />
-            <FoodCard food_image="" />
-            <FoodCard food_image="" />
-            <FoodCard food_image="" />
-            <FoodCard food_image="" />
-            <FoodCard food_image="" />
+            {images.map((image, index) => (
+              <FoodCard
+                key={index}
+                food_image={getImageLink(image)}
+                food_name={image.food_name}
+                food_full_name={image.food_full_name}
+                food_price={image.food_price}
+              />
+            ))}
           </div>
         </div>
       </section>
--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -1,12 +1,67 @@
+import { useState } from "react";
 import "./FoodCard.css";
+import addChartIcon from "../../assets/images/icon-add-to-cart.svg";
 
-export default function FoodCard({ food_image }) {
+export default function FoodCard({
+  food_image,
+  food_name,
+  food_full_name,
+  food_price,
+}) {
+  const [isClicked, setIsClicked] = useState(false);
+  const [count, setCount] = useState(0);
+
+  const incrementCount = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  const decrementCount = () => {
+    setCount((prevCount) => (prevCount > 0 ? prevCount - 1 : 0));
+  };
+
   return (
     <div className="food-card">
+      <div className="food-card__img">
+        <img src={food_image} alt="" className="food_img" />
+      </div>
       <div
-        className="food-card__img"
-        style={{ backgroundImage: `url(${food_image})` }}
-      ></div>
+        className="add-cart-container"
+        onClick={() => setIsClicked((prev) => !prev)}
+      >
+        {!isClicked ? (
+          <div className="add-cart-container__text">
+            <img src={addChartIcon} alt="Add to cart" />
+            <p>Add to Cart</p>
+          </div>
+        ) : (
+          <div className="add-cart-container__value">
+            <div
+              className="icon-crement add-cart-container__value__decrement"
+              onClick={(e) => {
+                e.stopPropagation(); 
+                decrementCount();
+              }}
+            >
+              -
+            </div>
+            <div className="add-cart-container__value__count">{count}</div>
+            <div
+              className="icon-crement add-cart-container__value__increment"
+              onClick={(e) => {
+                e.stopPropagation();
+                incrementCount();
+              }}
+            >
+              +
+            </div>
+          </div>
+        )}
+      </div>
+      <div className="food-details">
+        <h3 className="food_name">{food_name}</h3>
+        <p className="food_full_name">{food_full_name}</p>
+        <span className="food_price">{food_price}</span>
+      </div>
     </div>
   );
 }
